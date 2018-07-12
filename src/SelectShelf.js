9b2939934f7fc3cc4c7728fe4dc2946056b8b98e
Laura Logan <laurardh@aol.com>
2018-07-12 16:04:58 -0400

    adds handler function to SelectShelf

--- src/SelectShelf.js
+++ src/SelectShelf.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 
 
 class  SelectShelf extends Component {
+
+  handleChange(e) {
+    const selectedShelf = e.target.value;
+    this.props.onChangeShelf(selectedShelf);
+  }
+
+  constructor(props) {
+   super(props);
+
+   this.handleChange= this.handleChange.bind(this);
+ }
+ 
   render () {
     return (
       <div className="book-shelf-changer">

cafb68ff91a168548e0ddb845afbae2be71c4aa0
Laura Logan <laurardh@aol.com>
2018-07-24 12:44:03 -0400

    adds condition to Book for when no imageLink property

--- src/Book.js
+++ src/Book.js
@@ -24,7 +24,7 @@ class  Book extends Component {
       <div className="book">
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193,
-                          backgroundImage: `url(${book.imageLinks})` }}>
+                          backgroundImage: `url(${book.imageLinks.thumbnail !== undefined ? book.imageLinks.thumbnail : ''})` }}>
           </div>
           <SelectShelf  book={book}
                         books={this.props.books}
